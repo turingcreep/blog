75aee7816664efe9986a84a12d9a68563573f23c
Shankar Sengupta <shankar_sengupta@apple.com>
2018-04-03 13:07:24 -0700

    Adding prev/next functionality to navigator

--- src/components/navigator/index.js
+++ src/components/navigator/index.js
@@ -6,15 +6,41 @@ export default class Navigator extends Component {
         this.state = {
             cursor: 0
         };
+        this.previous = this.previous.bind(this);
+        this.next = this.next.bind(this);
     }
     
     render(){
         const {children} = this.props,
               {cursor} = this.state;
         return (<div className="navigator">
+                <nav>
+                    {this.state.cursor !== 0 &&
+                        <button type="button" className="navigator__previous" onClick={this.previous}>Previous</button>
+                    }
+                    {this.state.cursor !== this.props.children.length - 1 &&
+                    <button type="button" className="navigator__next" onClick={this.next}>Next</button>
+                    }
+                </nav>
                 {React.Children.map(children, (e,i) => {
                         return i===cursor? e: null;
                     })}
                 </div>);
+    }
+
+    previous(){
+        let cursor = this.state.cursor - 1;
+        this.setState({
+            ...this.state,
+            cursor
+        });
+    }
+
+    next(){
+        let cursor = this.state.cursor + 1;
+        this.setState({
+            ...this.state,
+            cursor
+        });
     }
 }
